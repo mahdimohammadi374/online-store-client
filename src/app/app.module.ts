18319503fa100e6d819ef0d8ff10e82a17a3f6e7
mahdimohammadi374 <mahdimohammadi374@gmail.com>
2024-07-01 18:38:55 +0330

    add bootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core/core.module";
+
+@NgModule({
+    declarations: [
+      AppComponent,
+    ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      SharedModule,
+      CoreModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+  })
+  export class AppModule { }
